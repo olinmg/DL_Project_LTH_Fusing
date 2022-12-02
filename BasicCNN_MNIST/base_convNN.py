Add ability to fuse models that have bias Fix bug in model weights averaging

BasicCNN_MNIST/base_convNN.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 from sklearn.model_selection import train_test_split
 from torchvision import datasets
@@ -59,7 +60,7 @@ def get_data_loaders(num_models):
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        bias = False # Fusion only works without bias (for the time being)
+        bias = True
         self.conv1 = nn.Sequential(         
             nn.Conv2d(
                 in_channels=1,              
@@ -89,6 +90,30 @@ class CNN(nn.Module):
         output = self.out(x)
         return output, x    # return x for visualization
 
+# 3. defining a basic CNN model
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        bias = True 
+
+        self.lin1 = nn.Sequential(
+            nn.Linear(28*28, 128, bias=bias),
+            nn.ReLU(),  
+        )
+        self.lin2 = nn.Sequential(
+            nn.Linear(128, 512, bias=bias),
+            nn.ReLU(),  
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(512, 10, bias=bias)
+    
+    def forward(self, x):
+
+        x = self.lin1(x.view(-1, 28*28))
+        x = self.lin2(x)  
+        output = self.out(x)
+        return output, x    # return x for visualization
+
 
 
 # 4. instantiating necessary objects: putting the pieces together
@@ -97,9 +122,9 @@ loss_func = nn.CrossEntropyLoss()
 
 # 5. define the training function
 num_epochs = 10
-def train(num_epochs, cnn_model, loaders, args):
-    optimizer = optim.Adam(cnn_model.parameters(), lr = 0.01)
-    cnn_model.train()
+def train(num_epochs, model, loaders, args):
+    optimizer = optim.Adam(model.parameters(), lr = 0.01)
+    model.train()
         
     # Train the model
     total_step = len(loaders['train'])
@@ -111,7 +136,7 @@ def train(num_epochs, cnn_model, loaders, args):
             # gives batch data, normalize x when iterate train_loader
             b_x = Variable(images)   # batch x
             b_y = Variable(labels)   # batch y
-            output = cnn_model(b_x)[0]               
+            output = model(b_x)[0]               
             loss = loss_func(output, b_y)
             
             # clear gradients for this training step   
@@ -146,6 +171,14 @@ def test(model, args):
     return accuracy_accumulated/total
 
 
+def get_model(model_name):
+    if model_name == "cnn":
+        return CNN()
+    elif model_name == "mlp":
+        return MLP()
+    else:
+        print("Invalid model name. Using CNN instead.")
+        return CNN()
 
 # 7. actually execute the training and testing
 if __name__ == '__main__':
@@ -153,8 +186,9 @@ if __name__ == '__main__':
     num_models = args.num_models
     loaders = get_data_loaders(num_models)
     
+    model_parent = get_model(args.model_name)
     for idx in range(num_models):
-        model = CNN()
+        model = copy.deepcopy(model_parent) if not args.diff_weight_init else get_model(args.model_name)
         if args.gpu_id != -1:
             model = model.cuda(args.gpu_id)
         train(num_epochs, model, {"train": loaders["train"][idx], "test": loaders["test"]}, args)

BasicCNN_MNIST/fusion.py
@@ -1,35 +1,42 @@
+import itertools
 import torch
 import numpy as np
 import ot
-from base_convNN import CNN
+from base_convNN import CNN, get_model
 #from scipy.optimize import linear_sum_assignment # Could accomplish the same as OT with Hungarian Algorithm
 
+# get_histogram creates uniform historgram, i.e. [1/cardinality, 1/cardinality, ...]
 def get_histogram(cardinality):
     return np.ones(cardinality)/cardinality # uniform probability distribution
 
 def normalize_vector(coordinates, eps=1e-9):
     norms = torch.norm(coordinates, dim=-1, keepdim=True)
-    print("stats of vecs are: mean {}, min {}, max {}, std {}".format(
-        norms.mean(), norms.min(), norms.max(), norms.std()
-    ))
     return coordinates / (norms + eps)
 
+# compute_euclidian_distance_matrix computes a matrix where c[i, j] corresponds to the euclidean distance of x[i] and y[j]
 def compute_euclidian_distance_matrix(x, y, p=2, squared=True): # For some reason TA prefers squared to be True
     x_col = x.unsqueeze(1)
     y_lin = y.unsqueeze(0)
     c = torch.sum((torch.abs(x_col - y_lin)) ** p, 2)
     if not squared:
-        print("dont leave off the squaring of the ground metric")
         c = c ** (1/2)
     return c
 
-def get_ground_metric(coordinates1, coordinates2):
+# get_ground_metric computes the cost matrix
+# if bias is present, the bias will be appended to the weight matrix and subsequently used to calculate the cost
+# cost matrix is based on the weights, not the activations
+def get_ground_metric(coordinates1, coordinates2, bias1, bias2):
+    if bias1 != None: # and bias2 == None
+        assert bias2 == None
+        coordinates1 = torch.cat((coordinates1, bias1.view(bias1.shape[0], -1)), 1)
+        coordinates2 = torch.cat((coordinates2, bias2.view(bias2.shape[0], -1)), 1)
     coordinates1 = normalize_vector(coordinates1)
     coordinates2 = normalize_vector(coordinates2)
     return compute_euclidian_distance_matrix(coordinates1, coordinates2)
 
+# create_network_from_params creates a network given the list of weights
 def create_network_from_params(args, param_list):
-    model = CNN()
+    model = get_model(args.model_name)
 
     assert len(list(model.parameters())) == len(param_list) # Assumption: We are fusing into a model of same architecture
 
@@ -47,7 +54,7 @@ def create_network_from_params(args, param_list):
 
     return model
 
-# Fusion with cost matrix based on weights, NOT ACTIVATIONS
+# fusion fuses two models with cost matrix based on weights, NOT ACTIVATIONS
 def fusion(networks, args, eps=1e-7):
     assert len(networks) == 2 # Temporary assert. Later we can do more models
 
@@ -61,13 +68,28 @@ def fusion(networks, args, eps=1e-7):
 
     avg_aligned_layers = []
     T_var = None
+    bias = False
+    bias0_weight, bias1_weight = (None, None)
 
     for idx, ((layer0_name, fc_layer0_weight), (layer1_name, fc_layer1_weight)) in \
             enumerate(zip(networks[0].named_parameters(), networks[1].named_parameters())):
         
+        if bias:
+            # If in the last layer we detected bias, this layer will be the bias layer we handled before, so we can just skip it
+            bias=False
+            continue
+
+        # Check if this current layer has a bias
+        if (idx != num_layers-1):
+            next_layer0, next_layer1 = next(itertools.islice(networks[0].named_parameters(), idx+1, None)), next(itertools.islice(networks[1].named_parameters(), idx+1, None))
+            bias = True if "bias" in next_layer0[0] else False
+            bias0_weight, bias1_weight = (next_layer0[1], next_layer1[1]) if bias else (None, None)
+        else:
+            bias = False
+        
         print("idx {} and layer {}".format(idx, layer0_name))
+        print("Bias: {}".format(bias))
         assert fc_layer0_weight.shape == fc_layer1_weight.shape
-        previous_layer_shape = fc_layer1_weight.shape
 
         mu_cardinality = fc_layer0_weight.shape[0]
         nu_cardinality = fc_layer1_weight.shape[0]
@@ -85,23 +107,26 @@ def fusion(networks, args, eps=1e-7):
             fc_layer0_weight_data = fc_layer0_weight.data
             fc_layer1_weight_data = fc_layer1_weight.data
 
-
         if idx == 0:
             if is_conv:
-                M = get_ground_metric(fc_layer0_weight_data.view(fc_layer0_weight_data.shape[0], -1),
-                                fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1))
+                # input to ground_metric has shape: (#out_channels, #in_channels*height*width)
+                fc_layer0_flattened = fc_layer0_weight_data.view(fc_layer0_weight_data.shape[0], -1)
+                fc_layer1_flattened =fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1)
+                M = get_ground_metric(fc_layer0_flattened,
+                                fc_layer1_flattened, bias0_weight, bias1_weight)
             else:
-                M = get_ground_metric(fc_layer0_weight_data, fc_layer1_weight_data)
+                M = get_ground_metric(fc_layer0_weight_data, fc_layer1_weight_data, bias0_weight, bias1_weight)
 
             aligned_wt = fc_layer0_weight_data
         else:
             if is_conv:
                 T_var_conv = T_var.unsqueeze(0).repeat(fc_layer0_weight_data.shape[2], 1, 1)
                 aligned_wt = torch.bmm(fc_layer0_weight_data.permute(2, 0, 1), T_var_conv).permute(1, 2, 0)
-
+                
                 M = get_ground_metric(
                     aligned_wt.contiguous().view(aligned_wt.shape[0], -1),
-                    fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1)
+                    fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1), 
+                    bias0_weight, bias1_weight
                 )
             else:
                 if fc_layer0_weight.data.shape[1] != T_var.shape[0]:
@@ -112,9 +137,10 @@ def fusion(networks, args, eps=1e-7):
                         T_var.unsqueeze(0).repeat(fc_layer0_unflattened.shape[0], 1, 1)
                     ).permute(1, 2, 0)
                     aligned_wt = aligned_wt.contiguous().view(aligned_wt.shape[0], -1)
+
                 else:
                     aligned_wt = torch.matmul(fc_layer0_weight.data, T_var)
-                M = get_ground_metric(aligned_wt, fc_layer1_weight)
+                M = get_ground_metric(aligned_wt, fc_layer1_weight, bias0_weight, bias1_weight)
 
 
         mu = get_histogram(mu_cardinality)
@@ -137,20 +163,26 @@ def fusion(networks, args, eps=1e-7):
             marginals = torch.ones(T_var.shape[0]) / T_var.shape[0]
         marginals = torch.diag(1.0/(marginals + eps))  # take inverse
         T_var = torch.matmul(T_var, marginals)
+
         # -----------------------------------------------------------------------
 
         # ---- Assumption: Past correction = True (Anything else doesn't really make sense?)
         t_fc0_model = torch.matmul(T_var.t(), aligned_wt.contiguous().view(aligned_wt.shape[0], -1))
+
         # ---------------------------------------------------------------------------------
         # t_fc0_model corresponds to aligned weights according to weights of model 1
 
         # Averaging of aligned weights (Could potentially also favor one model over the other)
-        geometric_fc = (t_fc0_model+ fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1))
+        geometric_fc = (t_fc0_model+ fc_layer1_weight_data.view(fc_layer1_weight_data.shape[0], -1))/2
 
         if is_conv and layer_shape != geometric_fc.shape:
             geometric_fc = geometric_fc.view(layer_shape)
 
         avg_aligned_layers.append(geometric_fc)
+        if bias:
+            t_bias_aligned = torch.matmul(T_var.t(), bias0_weight.view(bias0_weight.shape[0], -1)).flatten()
+            geometric_bias = (t_bias_aligned + bias1_weight)/2
+            avg_aligned_layers.append(geometric_bias)
 
     
     return create_network_from_params(args, avg_aligned_layers)

BasicCNN_MNIST/main.py
@@ -1,5 +1,5 @@
 from parameters import get_parameters
-from base_convNN import CNN
+from base_convNN import get_model
 import torch
 from fusion import fusion
 from sklearn.model_selection import train_test_split
@@ -33,7 +33,7 @@ def get_pretrained_models(args, num_models):
 
     for idx in range(num_models):
         state = torch.load("models/base_cnn_model_dict_weak_{}.pth".format(idx))
-        model = CNN()
+        model = get_model(args.model_name)
         model.load_state_dict(state)
         if args.gpu_id != -1:
             model = model.cuda(args.gpu_id)