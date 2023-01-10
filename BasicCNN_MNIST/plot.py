da2c6f2c2f17bd810d3eb95a7fdb563e1dc570b2
alexandertheus <alexander.theudahl@gmail.com>
2023-01-10 14:20:05 +0100

    Add plot function that creates plots for our paper

--- BasicCNN_MNIST/plot.py
+++ BasicCNN_MNIST/plot.py
@@ -1,88 +1,43 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import json
+import seaborn as sns
+import pandas as pd
 
 
+"""
+plot creates a plot with arbitrary many lines
 
-def plot(ax, results, num_epochs, label):
-    x_data = [i for i in range(num_epochs+1)]
-    y_data = [None]*(num_epochs+1)
-    for key, value in results.items():
-        y_data[int(key)] = value
-    
-    ax.plot(x_data, y_data, label=label)
-
-
-
-if __name__ == '__main__':
-    with open('./results_multi_sparsity.json', 'r') as f:
-            params = json.load(f)
-    
-    results = params["results"]
-    experiment = params["experiment_parameters"]
-
-    num_epochs = experiment["num_epochs"]
-
-    x = [i for i in range(num_epochs+1)]
-
-    fig, ax = plt.subplots()
-
-    x = params["experiment_parameters"]["sparsity"]
-
-    accuracy_pruned_and_fused = [r["vgg11"]["model_1"]["accuracy_pruned_and_fused"]["0"] for r in results]
-    ax.plot(x, accuracy_pruned_and_fused, label="accuracy_pruned_and_fused")
-
-    accuracy_pruned = [r["vgg11"]["model_1"]["accuracy_pruned"]["0"] for r in results]
-    ax.plot(x, accuracy_pruned, label="accuracy_pruned")
-
-    accuracy_pruned_and_fused_multi = [r["vgg11"]["model_1"]["accuracy_pruned_and_fused_multiple_sparsities"]["0"] for r in results]
-    ax.plot(x, accuracy_pruned_and_fused_multi, label="accuracy_multi_sparsity")
-
-    """
-    accuracy_multi_over_pruned = []
-    for i in range(len(accuracy_pruned)):
-        accuracy_multi_over_pruned.append(accuracy_pruned_and_fused_multi[i] - accuracy_pruned[i])
-    
-    ax.plot(x, accuracy_multi_over_pruned, label="Performance gain over pruning")
-
-    accuracy_multi_over_pruned_fused = []
-    for i in range(len(accuracy_pruned)):
-        accuracy_multi_over_pruned_fused.append(accuracy_pruned_and_fused_multi[i] - accuracy_pruned_and_fused[i])
-    ax.plot(x, accuracy_multi_over_pruned_fused, label="Performance gain over pruning and fusing")"""
-
-    vals = ax.get_yticks()
-    ax.set_yticklabels(['{:,.1%}'.format(x) for x in vals])
-
-    """
-    accuracy_pruned_and_fused = [r["vgg11"]["accuracy_PaF_all"]["0"] for r in results]
-    ax.plot(x, accuracy_pruned_and_fused, label="accuracy_Paf_all")"""
-
-
-
-    """
-
-    result = results[int(len(results)/2)+3]
-    sparsity = result["sparsity"]
-    print(sparsity)
-    prune_type = result["prune_type"]
-    print(prune_type)
-    model_name = experiment["models"][0]["name"]
-
-    result = result[model_name]
-
-
-    plot(ax, result["accuracy_PaF"], num_epochs, label="PaF")
-    plot(ax, result["accuracy_FaP"], num_epochs, label="FaP")
-    plot(ax, result["model_0"]["accuracy_pruned"], num_epochs, label="Pruned")
-    plot(ax, result["model_0"]["accuracy_pruned_and_fused"], num_epochs, label="Pruned&Fused")"""
-    ax.legend(loc = 'upper right')
-
-    plt.show()
-
-
+lines: list of y-values of your lines, e.g. [[0.1, 0.3, 0.2], [0.5, 1.3, 1.9]]
+x: a list containing the values on the x-axis, e.g. [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9] (They will be shown as percentages in the final plot)
+labels: A list containing the labels of your lines, e.g. ["PaF", "FaP"]
+xlabel: The label of the x-axis
+ylabel: The label of the y-axis
+"""
+def plot(lines, x, labels, xlabel, ylabel):
+    sns.set_style('whitegrid') # darkgrid, white grid, dark, white and ticksplt.rc('axes', titlesize=18)     # fontsize of the axes title
+    labelsize=20
+    plt.rc('axes', labelsize=labelsize)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=labelsize)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=labelsize)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=labelsize)    # legend fontsize
+    plt.rc('font', size=labelsize)          # controls default text sizes
+    plt.rc("lines", markersize=10)
+    plt.rc("lines", linewidth=4)
 
     
+    plt.figure(figsize=(10,6), tight_layout=True)
+
+    dict = {}
+    for i in range(len(lines)):
+        dict[labels[i]] = lines[i]
+    dict[xlabel] = x
 
 
+    data = pd.DataFrame(dict)
 
-    
+    ax = sns.lineplot(data=pd.melt(data, [xlabel]), x=xlabel, y="value", hue="variable", marker = "o")
+    ax.set_yticklabels(['{:,.1%}'.format(y) for y in ax.get_yticks()])
+    ax.set_xticklabels(['{:,.0%}'.format(spa) for spa in ax.get_xticks()])
+    ax.set(xlabel=xlabel, ylabel=ylabel)
+    ax.legend(title_fontsize = 13)
+    #plt.show()
+    plt.savefig("./plots/example.svg", dpi=1200)
