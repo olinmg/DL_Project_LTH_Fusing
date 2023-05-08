9b9c4304df0db370906fac86f876f7a053995e25
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-08 16:16:02 +0200

    some

--- fusion_pruning_experiments/prune_resnet50_imagenet.py
+++ fusion_pruning_experiments/prune_resnet50_imagenet.py
@@ -44,13 +44,15 @@ from performance_tester_utils import (
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-gpu_id = -1
-model_name = "cnn_diff_weight_init_False_mnist_1"
-model_path = f"./models/models_cnn/{model_name}"
-dataset_name = "mnist"
+gpu_id = 0
+model_name = "resnet18"
+model_file = "resnet18_withBN_diff_weight_init_False_cifar10_eps300_1"
+model_path = f"./models/models_resnet18/{model_file}"
+dataset_name = "cifar10"
+
 
 print("Loading model ...")
-resnet50_model = get_pretrained_model_by_name(model_path, gpu_id=0)
+loaded_model = get_pretrained_model_by_name(model_path, gpu_id=0)
 
 print("Loading dataset ...")
 example_input = None
@@ -61,13 +63,16 @@ elif dataset_name == "cifar10":
     loaders = get_cifar10_data_loader()
     output_dim = 10
     example_input = torch.randn(1, 3, 32, 32)
+    eval_func = evaluate_performance_simple
 elif dataset_name == "cifar100":
     loaders = get_cifar100_data_loader()
     output_dim = 100
+
 elif dataset_name == "imagenet":
     loaders = get_imagenet_data_loader()
     output_dim = 1000
     example_input = torch.randn(1, 3, 224, 224)
+    eval_func = evaluate_performance_imagenet
 
 prune_params = {
     "prune_type": "l1",
@@ -79,7 +84,7 @@ prune_params = {
     "prune_iter_epochs": 10,
     "loaders": loaders,
     "gpu_id": gpu_id,
-    "model_name": "resnet50",
+    "model_name": model_name,
     "use_intrafusion_for_pruning": False,
 }
 
@@ -91,7 +96,7 @@ params = {
         gpu_id=gpu_id,
         num_samples=200,
     ),
-    "eval_function": evaluate_performance_imagenet,
+    "eval_function": eval_func,
     "loaders": loaders,
     "gpu_id": gpu_id,
 }
@@ -102,30 +107,36 @@ model_accuracy_development = {}
 # prune the model - possibly iteratively
 print("Starting the (iterative) pruning ...")
 pruned_model_lis, pruned_model_accuracies, _ = pruning_test_manager(
-    input_model_list=[resnet50_model], prune_params=prune_params, **params
+    input_model_list=[loaded_model], prune_params=prune_params, **params
 )
 pruned_model = pruned_model_lis[0]
 # store the model after iterative pruning
 iterprune_text = f"{prune_params['prune_iter_steps']}iter{prune_params['prune_iter_epochs']}"
 torch.save(pruned_model, filename=f"{model_path}_{iterprune_text}.pth.tar")
 model_accuracy_development["pruning_accuracies"] = pruned_model_accuracies
-with open(f"pruning_accuracies_{model_name}.json", "w") as outfile:
+with open(f"pruning_accuracies_{model_file}.json", "w") as outfile:
     json.dump(model_accuracy_development, outfile, indent=4)
 print(f"Model pruning is done. Final accuracy: {pruned_model_accuracies[-1]}")
 
 # additional retraining of the model
 retrain_epochs = 0
 print(f"Starting additional training for {retrain_epochs} epochs ...")
-retrained_pruned_model, val_acc_per_epoch = train_during_pruning_resnet50(
-    pruned_model, loaders, retrain_epochs, gpu_id, prune=False, performed_epochs=0
+retrained_pruned_model, val_acc_per_epoch = train_during_pruning(
+    pruned_model,
+    loaders,
+    retrain_epochs,
+    gpu_id,
+    prune=False,
+    performed_epochs=0,
+    model_name=model_name,
 )
 torch.save(
     retrained_pruned_model, filename=f"{model_path}_{iterprune_text}_T{retrain_epochs}.pth.tar"
 )
 model_accuracy_development["retraining_accuracies"] = val_acc_per_epoch
 
-with open(f"retraining_accuracies_{model_name}.json", "w") as outfile:
+with open(f"retraining_accuracies_{model_file}.json", "w") as outfile:
     json.dump(model_accuracy_development, outfile, indent=4)
 model_accuracy_development["all_accuracies"] = pruned_model_accuracies.extend(val_acc_per_epoch)
-with open(f"overall_pruning_retraining_accuracies_{model_name}.json", "w") as outfile:
+with open(f"overall_pruning_retraining_accuracies_{model_file}.json", "w") as outfile:
     json.dump(model_accuracy_development, outfile, indent=4)
