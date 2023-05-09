494abf3636bfea5971299c2d8ad26199ab808db3
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-10 00:34:03 +0200

    sd

--- fusion_pruning_experiments/train_resnet50.py
+++ fusion_pruning_experiments/train_resnet50.py
@@ -301,7 +301,7 @@ def main_worker(gpu, ngpus_per_node, args):
                 # best_acc1 may be from a checkpoint from a different GPU
                 best_acc1 = best_acc1.to(args.gpu)
             # model.load_state_dict(checkpoint["state_dict"])
-            model = torch.load(f"{args.resume}.pth")
+            model = torch.load(f"{args.resume.split('.')[0]}.pth")
             if torch.cuda.is_available():
                 model.cuda()
             optimizer.load_state_dict(checkpoint["optimizer"])
