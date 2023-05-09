e3549a2917323f3f65187a83d272da8fbb0fccbc
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-10 01:26:31 +0200

    a

--- fusion_pruning_experiments/prune_resnet50_imagenet.py
+++ fusion_pruning_experiments/prune_resnet50_imagenet.py
@@ -113,7 +113,7 @@ def prune_structured_resnet50(
     accuarcies_between_prunesteps = []
     for i in range(prune_iter_steps):  # iterative pruning
         print(f"\n{i}")
-
+        print(model.modules())
         for m in model.modules():
             if isinstance(m, torch.nn.Linear) and m.out_features == out_features:
                 ignored_layers.append(m)
