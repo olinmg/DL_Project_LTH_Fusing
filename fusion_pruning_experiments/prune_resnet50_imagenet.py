042f4ccd977e15b99dbc1bf4d7cdd59bd71d6f68
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-10 17:39:53 +0200

    changing some of original load model code

--- fusion_pruning_experiments/prune_resnet50_imagenet.py
+++ fusion_pruning_experiments/prune_resnet50_imagenet.py
@@ -272,16 +272,14 @@ model_path = f"./models/models_resnet50/{model_file}"
 dataset_name = "imagenet"
 
 print(f"Loading resnet50 model: {model_path}")
-"""
-# loaded_model = get_pretrained_model_by_name(model_path, gpu_id=0)
+
 loaded_model = model_archs.__dict__["resnet50"]()
 loaded_model = torch.nn.DataParallel(loaded_model)
 checkpoint = torch.load(f"{model_path}.pth.tar")
 loaded_model.load_state_dict(checkpoint["state_dict"])
-loaded_model = loaded_model.module.to("cpu")
-loaded_model = loaded_model.cuda(gpu_id)
-"""
-loaded_model = torch.load(f"{model_path}.pth")
+# loaded_model = loaded_model.module.to("cpu")
+# loaded_model = loaded_model.cuda(gpu_id)
+
 loaded_model = nn.DataParallel(loaded_model)
 loaded_model.to(device)
 
