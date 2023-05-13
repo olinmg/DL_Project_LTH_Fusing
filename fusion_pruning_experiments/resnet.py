c240d559bd5d3eeaaa5a374122abcac1d1efa44c
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-13 12:44:09 +0200

    adjusted Bottleneck to not get linear_bias arg

--- fusion_pruning_experiments/resnet.py
+++ fusion_pruning_experiments/resnet.py
@@ -1,25 +1,27 @@
-'''ResNet in PyTorch.
+"""ResNet in PyTorch.
 
 For Pre-activation ResNet, see 'preact_resnet.py'.
 
 Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Deep Residual Learning for Image Recognition. arXiv:1512.03385
-'''
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.autograd import Variable
 
 # Taken from https://github.com/kuangliu/pytorch-cifar
+
 
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1, use_batchnorm=False, linear_bias=True):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=linear_bias)
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=linear_bias
+        )
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=linear_bias)
         self.bn2 = nn.BatchNorm2d(planes)
@@ -31,12 +33,18 @@ class BasicBlock(nn.Module):
             self.bn1 = self.bn2 = nn.Sequential()
 
         self.shortcut = nn.Sequential()
-        #if stride != 1 or in_planes != self.expansion*planes:
-        if stride != 1 or in_planes != self.expansion*planes:
+        # if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             print("Need shortcut")
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=linear_bias),
-                nn.BatchNorm2d(self.expansion*planes) if use_batchnorm else nn.Sequential()
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=linear_bias,
+                ),
+                nn.BatchNorm2d(self.expansion * planes) if use_batchnorm else nn.Sequential(),
             )
         else:
             print("No shortcut")
@@ -58,17 +66,19 @@ class Bottleneck(nn.Module):
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
 
         if not use_batchnorm:
             self.bn1 = self.bn2 = self.bn3 = nn.Sequential()
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes) if use_batchnorm else nn.Sequential()
+                nn.Conv2d(
+                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(self.expansion * planes) if use_batchnorm else nn.Sequential(),
             )
 
     def forward(self, x):
@@ -93,20 +103,31 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes, bias=linear_bias)
+        self.linear = nn.Linear(512 * block.expansion, num_classes, bias=linear_bias)
 
         self.relu1 = nn.ReLU(inplace=False)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride, self.use_batchnorm, linear_bias = self.linear_bias))
+            if isinstance(block, Bottleneck):
+                layers.append(block(self.in_planes, planes, stride, self.use_batchnorm))
+            else:
+                layers.append(
+                    block(
+                        self.in_planes,
+                        planes,
+                        stride,
+                        self.use_batchnorm,
+                        linear_bias=self.linear_bias,
+                    )
+                )
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out =F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
