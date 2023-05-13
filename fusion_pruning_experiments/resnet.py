ae484dd7ea0cf0fe6575a2de70e653d39ad91c11
olinmg <77267475+olinmg@users.noreply.github.com>
2023-05-13 12:58:04 +0200

    added linear_bias to bottleneck init

--- fusion_pruning_experiments/resnet.py
+++ fusion_pruning_experiments/resnet.py
@@ -60,7 +60,7 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1, use_batchnorm=False):
+    def __init__(self, in_planes, planes, stride=1, use_batchnorm=False, linear_bias=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
